846ccf70fb89d5c72a1e0e0ec17995317f69f7ca
unknown <email@example.com>
2018-09-11 21:47:29 +0800

    init

---
+++ homework1/promgram1/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("输入第一个数字：");
+            string s = Console.ReadLine();
+            double a = double.Parse(s);
+            Console.Write("输入第二个数字：");
+            string n = Console.ReadLine();
+            double b = double.Parse(n);
+            Console.WriteLine("它们的积为：" + (a * b));
+        }
+    }
+}
---
+++ homework1/promgram2/ConsoleApp2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string s;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            string n = textBox2.Text;
+            double num1 = double.Parse(s);
+            double num2 = double.Parse(n);
+            double num = num1 * num2;
+            textBox3.Text = "乘积：" + num;
+        }
+    }
+}
