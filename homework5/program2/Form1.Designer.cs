45e4941943eaf9ccb9b3fc45782b3d1da26594fe
unknown <email@example.com>
2018-10-17 22:46:18 +0800

    init

--- homework5/program2/Form1.Designer.cs
+++ homework5/program2/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.SuspendLayout();
             // 
             // button1
@@ -44,7 +45,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(688, 53);
+            this.button2.Location = new System.Drawing.Point(688, 105);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -69,6 +70,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
     }
 }
 
--- homework5/program2/Form1.cs
+++ homework5/program2/Form1.cs
@@ -24,7 +24,14 @@ namespace program2
             {
                 graphics = this.CreateGraphics();
             }
-            drawCayleyTree(10, 300, 400,100, -Math.PI / 2);
+            ColorDialog ColorForm = new ColorDialog();
+            if (ColorForm.ShowDialog() == DialogResult.OK)
+            {
+               Color color = ColorForm.Color;
+                //GetColor就是用户选择的颜色
+                drawCayleyTree(color, 10, 300, 400, 100, -Math.PI / 2);
+            }
+               
             button1.Enabled = false;
            
         }
@@ -34,7 +41,7 @@ namespace program2
             ReScree();
             button1.Enabled = true;
         }
-
+        
 
         private Graphics graphics;
         double th1 = 30 * Math.PI / 180;
@@ -48,28 +55,37 @@ namespace program2
         }
 
 
-        void drawCayleyTree(int n, double x0, double y0, double leng, double th)
+        void drawCayleyTree( Color color,int n, double x0, double y0, double leng, double th)
         {
             if (n == 0) return;
 
             double x1 = x0 + leng * Math.Cos(th);
             double y1 = y0 + leng * Math.Sin(th);
 
-            drawLine(x0, y0, x1, y1,n/2);
+            drawLine(color,x0, y0, x1, y1,n/4);
 
-            drawCayleyTree(n - 1, x1, y1, per1 * leng*(0.5 + rand()), th + th1 * (0.5 + rand()));
-            drawCayleyTree(n - 1, x1, y1, per2 * leng*(0.4 + rand()), th - th2 * (0.5 + rand()));
+            drawCayleyTree(color,n - 1, x1, y1, per1 * leng*(0.5 + rand()), th + th1 * (0.5 + rand()));
+            drawCayleyTree(color,n - 1, x1, y1, per2 * leng*(0.4 + rand()), th - th2 * (0.5 + rand()));
         }
-        void drawLine(double x0,double y0,double x1,double y1,int width)
+
+        void drawLine(Color GetColor,double x0,double y0,double x1,double y1,int width)
         {
-            graphics.DrawLine(
-           new Pen(Color.Red,width),
-            (int)x0,(int)y0,(int)x1,(int)y1);
+           
+                graphics.DrawLine(
+           new Pen(color:GetColor, width: width),
+            (int)x0, (int)y0, (int)x1, (int)y1);           
+           
+            
         }
 
         private void ReScree()
         {
             this.Invalidate();//使窗口重绘
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
